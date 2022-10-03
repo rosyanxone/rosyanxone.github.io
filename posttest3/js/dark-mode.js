2a0f4e1b2c7b59a9e76161272fb4f507b4e186ea
rosyanxone <rausyanfikrkarmayoga@gmail.com>
2022-10-03 13:30:56 +0800

    posttest3

--- posttest3/js/dark-mode.js
+++ posttest3/js/dark-mode.js
@@ -1,14 +1,4 @@
-const chk = document.getElementById('chk');
-chk.addEventListener('change', result);
-
-function result() {
-    let darkDiv = document.querySelectorAll('.mode');
-    let lightImg = document.querySelectorAll('.mode-img');
-    
-    for(let x of darkDiv) {
-        x.classList.toggle('dark')
-    }
-    for(let y of lightImg) {
-        y.classList.toggle('light-img');
-    }
-}
+$('#chk').change(() => {
+    $('.mode').toggleClass('dark'); 
+    $('.mode-img').toggleClass('light-img');
+});
